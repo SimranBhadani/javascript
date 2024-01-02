44737f450a77ea8cd5899f4669b3f66f5764d8ce
Simran Bhadani <144092744+SimranBhadani@users.noreply.github.com>
2024-01-02 14:42:18 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score="55abd"
+ 
+ // convert to number
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(valueInNumber); 
+console.log(typeof valueInNumber);
+
+//note:-
+//"55"=> convert to number
+//"55abc"=>NAN
+// true=> 1 and false=> 0
+ 
+// convert to boolean
+let Ispresent=1; // number
+let booleanIspresent=Boolean(Ispresent); // convert to boolean
+console.log(typeof booleanIspresent); // boolean
+console.log(booleanIspresent); //true
+
+// 1=> true and 0=> false
+//""=>false
+//"sim"=>false
+
+// convert to string
+let num=556;
+let stringNumber=String(num); //converted to string
+console.log(stringNumber); 556
+console.log(typeof stringNumber); // string
+
+
+
+
+
