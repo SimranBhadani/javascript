e0403f38591c49d6b1740ee136fb957d65cec7c9
Simran Bhadani <144092744+SimranBhadani@users.noreply.github.com>
2024-01-02 13:06:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+// datatypes
+
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(4+4) // code readibility is important 
+
+let name="simran"
+let marks=92
+let isLoggedIn=true
+
+console.log(name)
+ 
+// Datatypes in javascript
+
+// number => range(2 to power 53)
+//bigint
+//string=""
+//boolean=true/false
+//null=>standalone value
+//undefined
+// symbol=> unique
+
+//object
+ 
+// typeof=> says about datatype of variable
+console.log(typeof "simran")
+console.log(typeof isLoggedIn)
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
